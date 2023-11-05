b63aef8eb98d18b2a5933be31eb2a4a6c0c247bf
erm111 <80110106+erm111@users.noreply.github.com>
2023-11-05 14:27:19 +0100

    Update footer.php

--- student_clearance_system_Aurthur_Javis/footer.php
+++ student_clearance_system_Aurthur_Javis/footer.php
@@ -7,6 +7,6 @@
 -->
 </style>
  <div class="footer">
-          <div class="style1"> <p> Copyright @ <?php echo date("Y")?>. EKE, EMMANUEL EFA-EVAL | 18/132010 | Online clearance system| Aurthur Javis University, Calabar.  All Rights Reserved. </p>
+          <div class="style1"> <p> Copyright @ <?php echo date("Y")?>. EKE, EMMANUEL EFA-EVAL | | Online clearance system| Aurthur Javis University, Calabar.  All Rights Reserved. </p>
 </div>
 </div>
