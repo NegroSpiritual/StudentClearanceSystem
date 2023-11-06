d82cf48a5f3c06372bf236e0468f657895a58e3d
erm111 <uzomannanyere@gmail.com>
2023-11-06 01:14:47 +0100

    profile and student clearance

--- student_clearance_system_Aurthur_Javis/profile.php
+++ student_clearance_system_Aurthur_Javis/profile.php
@@ -51,20 +51,38 @@ $current_date = date('Y-m-d H:i:s');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Arthur Jarvis University Student</title>
+    <title>Arthur Jarvis University Student Profile</title>
+    
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
 
-    <style>
-        /* Custom CSS for color */
+    <!-- Morris -->
+    <link href="css/plugins/morris/morris-0.4.3.min.css" rel="stylesheet">
+
+    <!-- Gritter -->
+    <link href="js/plugins/gritter/jquery.gritter.css" rel="stylesheet">
+
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+<link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+<style type="text/css">
+<!--
+.style1 {color: #000000}
+.style2 {color: #FF0000}
+-->
+</style>
+<style>
+        /* Custom CSS for color and layout */
         body {
-            background-color: #ffffff; 
+            background-color: #ffffff;
         }
         .custom-green {
-            color: #008000; 
+            color: #008000;
         }
         .profile-page {
-            text-align: center; 
-            margin: 0 auto; 
-            max-width: 500px; 
+            text-align: center;
+            margin: 0 auto;
+            max-width: 500px;
             font-size: large;
             justify-content: space-around;
         }
@@ -80,37 +98,122 @@ $current_date = date('Y-m-d H:i:s');
             justify-content: space-around;
             align-items: stretch;
         }
-        .details-page{
-          display: flex;
+        .details-page {
+            display: flex;
         }
-        .image-page{
-           display: flex;
-
+        .image-page {
+            display: flex;
         }
-        .span-text{
-           text-align: left;
+        .span-text {
+            text-align: left;
+        }
+        table {
+            width: 100%;
+            background-color: #337ab7;
+            color: white;
+        }
+        table, th, td {
+            border: 1px solid #337ab7;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #337ab7;
         }
     </style>
 </head>
 <body>
-   <div class="profile-page"> 
+<div id="wrapper">
+
+<nav class="navbar-default navbar-static-side" role="navigation">
+    <div class="sidebar-collapse">
+        <ul class="nav metismenu" id="side-menu">
+            <li class="nav-header">
+                <div class="dropdown profile-element"> <span>
+                        <img src="<?php echo $rowaccess['photo'];  ?>" alt="image" width="142" height="153" class="img-circle" />
+                         </span>
+
+
+                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                        <span class="clear"><span class="text-muted text-xs block">Matric No:<?php echo $rowaccess['matric_no'];  ?> <b class="caret"></b></span> </span> </a>
+                    <ul class="dropdown-menu animated fadeInRight m-t-xs">
+                        
+                        <li><a href="logout.php">Logout</a></li>
+                    </ul>
+</div>	
+           <?php
+           include('sbar.php');
+           
+           ?>
+           
+       </ul>
+       
+
+    </div>
+</nav>
+<div id="page-wrapper" class="gray-bg">
+        <div class="row border-bottom">
+        <nav class="navbar navbar-static-top white-bg" role="navigation" style="margin-bottom: 0">
+        <div class="navbar-header">
+            <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
+            
+        </div>
+            <ul class="nav navbar-top-links navbar-right">
+                <li>
+				
+				
+                    <span class="m-r-sm text-muted welcome-message">Welcome <?php echo $rowaccess['fullname']; ?></span>
+                </li>
+                <li class="dropdown">
+                   
+                    
+
+
+                <li>
+                    <a href="logout.php">
+                        <i class="fa fa-sign-out"></i> Log out
+                    </a>
+                </li>
+               
+            </ul>
+
+        </nav>
+        </div>
+<div class="wrapper wrapper-content">
+<div class="profile-page">
         <form id="form-profile" class="form-page">
-        <span>
-            <div class="image-page">
-        <img src="<?php echo $rowaccess['photo']; ?>" alt="image" width="150" height="175" class="img-circle" />
-       </span> 
-       </div>
-     <div id= "details-profile" class ="details-page">
-          <span class="span-text">
-          <p class="custom-green">NAME OF STUDENT: <?php echo $rowaccess['fullname'] . "<br>"; ?></p>
-            <p class="custom-green">MATRIC NUMBER: <?php echo $rowaccess['matric_no'] . "<br>"; ?> </p>
-            <p class="custom-green">FACULTY: <?php echo $rowaccess['faculty'] . "<br>"; ?> </p>
-            <p class="custom-green">DEPARTMENT: <?php echo $rowaccess['dept'] . "<br>"; ?> </p>
-            <p class="custom-green">PHONE NUMBER: <?php echo $rowaccess['phone'] . "<br>"; ?> </p>
-          </span>        
-     </div>
+          
+            <table>
+                <tr>
+                    <th colspan="2">Student Details</th>
+                </tr>
+                <tr>
+                    <td class="custom-green">Name of Student:</td>
+                    <td><?php echo $rowaccess['fullname']; ?></td>
+                </tr>
+                <tr>
+                    <td class="custom-green">Matric Number:</td>
+                    <td><?php echo $rowaccess['matric_no']; ?></td>
+                </tr>
+                <tr>
+                    <td class="custom-green">Faculty:</td>
+                    <td><?php echo $rowaccess['faculty']; ?></td>
+                </tr>
+                <tr>
+                    <td class="custom-green">Department:</td>
+                    <td><?php echo $rowaccess['dept']; ?></td>
+                </tr>
+                <tr>
+                    <td class="custom-green">Phone Number:</td>
+                    <td><?php echo $rowaccess['phone']; ?></td>
+                </tr>
+            </table>
         </form>
-  </div>
+    </div>
 </body>
 </html>
 
+<?php include ('footer.php'); ?>
---
+++ student_clearance_system_Aurthur_Javis/sbar.php
@@ -0,0 +1,5 @@
+<li class="nav-header">
+                </li>
+                <li class="active">
+                <a href="index.php"><i class="fa fa-th-large"></i> <span class="nav-label">Dashboard</span></a></li>
+				<li>
